53740274eb38edfe0c5ef6b857c76680f506875b
SaiqaJat <saaiqqaa@gmail.com>
2026-04-22 13:14:37 +0500

    Add files via upload

---
+++ schemas.sql
@@ -0,0 +1,32 @@
+--NETFLIX PROJECT
+DROP TABLE IF EXISTS netflix;
+CREATE TABLE netflix(
+		show_id VARCHAR(10),
+		type VARCHAR(10),
+		title VARCHAR(150),
+		director VARCHAR(210),
+		casts VARCHAR(1000),
+		country  VARCHAR(150),
+		date_added  VARCHAR(50),
+		release_year INT,
+		rating  VARCHAR(10),
+		duration  VARCHAR(15),
+		listed_in  VARCHAR(100),
+		description  VARCHAR(250)
+
+);
+
+SELECT * FROM netflix;
+
+SELECT 
+	COUNT(*) as total_content
+FROM netflix;
+
+
+
+
+
+
+
+
+
